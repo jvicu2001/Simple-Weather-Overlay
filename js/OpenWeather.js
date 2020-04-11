6952542a7b6c1b245de5bb0c8e67fe7658b5df63
Jorge Vicuña <jvicuna2001@gmail.com>
2020-04-11 02:23:37 -0400

    More accurate min and max temperatures
    OpenWeather released their new "One Call API" that allows SWO to get the actual daily forecast.
    This was not possible before with the "Hourly Forecast" API that OpenWeather offers.

--- js/OpenWeather.js
+++ js/OpenWeather.js
@@ -5,6 +5,10 @@ var lastCondition = {
 	min: "",
 	max: ""
 };
+var placeCoordinates = {
+	lat: "",
+	lon: ""
+}
 $(document).ready(function(){
 	var parameters = new URL(window.location.href).searchParams;
 	var API_key = parameters.get("api_key");
@@ -32,9 +36,10 @@ $(document).ready(function(){
 	var lang = parameters.get("lang");
 	var showPlace = parameters.get("show-place");
 	var fullDescription = parameters.get("full-description");
+	var midday = 0;
 		
 	
-	function CurrentData() {
+	function PlaceData() {
 		$.ajax({
 			url: "https://api.openweathermap.org/data/2.5/weather",
 			data: {
@@ -47,43 +52,24 @@ $(document).ready(function(){
 			dataType: "json"
 		})
 		.done(function( json ) {
-			var condition_type = "";
-			if (fullDescription === "on") {
-				condition_type = json.weather[0].description;
-			}
-			else {
-				condition_type = json.weather[0].main;
-			}
-			if (json.weather[0].icon !== lastCondition.icon) {
-				$( "#icon" ).attr( "src", "https://openweathermap.org/img/wn/" + json.weather[0].icon + "@2x.png" );
-				lastCondition.icon = json.weather[0].icon;
-			}
-			if (condition_type !== lastCondition.condition) {
-				$( ".WeatherCondition" ).text(condition_type);
-				lastCondition.condition = condition_type;
-			}
-			if (json.main.temp !== lastCondition.current) {
-				$( ".CurrentTemp" ).text(Math.round(json.main.temp) + unitsSymbol);
-				lastCondition.current = json.main.temp;
-			}
+			placeCoordinates = json.coord;
 			if (showPlace === "on") {
 				$("#PlaceName").text(json.name);
 			}
+			CurrentData();
 
 		})
 		.fail(function( json ) {
-			console.log(json.message);
+			console.log("city not found");
 			$( ".CurrentTemp" ).text("City not found.");
-		})
-		.always(function() {
-				setTimeout(CurrentData, 5*60000);
 		});
-	}
-	function DailyData() {
+	}	
+	function CurrentData() {
 		$.ajax({
-			url: "https://api.openweathermap.org/data/2.5/forecast",
+			url: "https://api.openweathermap.org/data/2.5/onecall",
 			data: {
-				"q": place,
+				"lat": placeCoordinates.lat,
+				"lon": placeCoordinates.lon,
 				"APPID": API_key,
 				"units": units,
 				"lang": lang
@@ -92,32 +78,41 @@ $(document).ready(function(){
 			dataType: "json"
 		})
 		.done(function( json ) {
-
-			var allMin = [];
-			var allMax = [];
-			/* var allMin = json.list.slice(0, 7).map(function(i){return parseFloat(i.main.temp_min);});
-			var allMax = json.list.slice(0, 7).map(function(i){return parseFloat(i.main.temp_min);}); */
-			for (i = 0; i < 8; i++) {
-				allMin.push(json.list[i].main.temp_min);
-				allMax.push(json.list[i].main.temp_max);
+			var condition_type = "";
+			if (fullDescription === "on") {
+				condition_type = json.current.weather[0].description;
 			}
-			var temp_min = Math.min(...allMin);
-			var temp_max = Math.max(...allMax);
-			if (temp_min !== lastCondition.min) {
-				$( ".MinTemp" ).html("Min " + Math.round(temp_min) + unitsSymbol);
-				lastCondition.min = temp_min;
+			else {
+				condition_type = json.current.weather[0].main;
 			}
-			if (temp_max !== lastCondition.max) {
-				$( ".MaxTemp" ).html("Max " + Math.round(temp_max) + unitsSymbol);
-				lastCondition.max = temp_max;
+			if (json.current.weather[0].icon !== lastCondition.icon) {
+				$( "#icon" ).attr( "src", "https://openweathermap.org/img/wn/" + json.current.weather[0].icon + "@2x.png" );
+				lastCondition.icon = json.current.weather[0].icon;
 			}
-
-			
+			if (condition_type !== lastCondition.condition) {
+				$( ".WeatherCondition" ).text(condition_type);
+				lastCondition.condition = condition_type;
+			}
+			if (json.current.temp !== lastCondition.current) {
+				$( ".CurrentTemp" ).text(Math.round(json.current.temp) + unitsSymbol);
+				lastCondition.current = json.current.temp;
+			}
+			if (json.daily[0].dt > (midday + 43200) || lastCondition.min === ""){
+				/*
+				The "dt" value indicates the current day's midday time in epoch, and it (should) change when the day passes.
+				Because of this, we determine when to get the next day's data when the "dt" value changes at least by 12 hours,
+				or if there was no previous data.
+				*/
+				$( ".MinTemp" ).text(Math.round(json.daily[0].temp.min) + unitsSymbol);
+				$( ".MaxTemp" ).text(Math.round(json.daily[0].temp.max) + unitsSymbol);
+				lastCondition.min = json.daily[0].temp.min;
+				lastCondition.max = json.daily[0].temp.max;
+			}
 		})
 		.always(function() {
-				setTimeout(CurrentData, 86400000); //Check everyday (Why would you stream for more than a day?)
+				setTimeout(CurrentData, 5*60000);
 		});
 	}
-	CurrentData();
-	DailyData();
+	
+	PlaceData();
 });
